d55f4abba3b4306348bbd5ef197edc6b946c2f9b
Vijay A <vijayadhav111@gmail.com>
2021-10-13 16:56:20 +0530

    sub function

--- calc.py
+++ calc.py
@@ -2,4 +2,9 @@ print('---------------------- test project -------------------------------')
 def add(a, b):
     return a + b
 
+def sub(a, b):
+    return a - b
+
+
 print(add(10, 10))
+print(sub(50, 10))
